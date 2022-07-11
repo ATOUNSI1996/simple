292b694f4962377fbfada1cfca333fc213c1f337
ATOUNSI1996 <tounsi.ahmed96@gmail.com>
2022-07-11 17:47:25 +0100

    TP3Ex4

---
+++ TP3Ex4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int puissance(int x, int n)
+{
+    int res = 1;
+    res = pow(x, n);
+    return res;
+}
+
+int main() {
+   int x,n;
+    printf("entrer la valeur de x \n");
+    scanf("%d",&x);
+    printf("entrer la valeur de n \n");
+    scanf("%d",&n);
+    printf("la valeur de %d puissance %d est %d",x,n,puissance(x,n)); 
+
+   
+}
---
+++ TP3_Ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+   int i = 1;
+    while((i%i == 0) && (i % 1 == 0) )
+    {
+        printf("le nombre %d est premier ",i);
+        i++;
+    }
+
+    
+}
