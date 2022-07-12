556d4e3585df505af369f5ace2cc31ef39c18da6
ATOUNSI1996 <tounsi.ahmed96@gmail.com>
2022-07-12 11:04:43 +0100

    tables

---
+++ TAB1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i;
+    //Declaration d'un tableau
+   int tab [5];
+ 
+   for(i = 0; i < 5; i++)
+   {
+       printf("entrer le %d element \n ",i);
+       scanf("%d ",&tab[i]);
+   }
+    
+    for(i = 0; i < 5; i++)
+    {
+        printf("tab : ",tab[i]);
+    }
+
+    int min = tab[0];
+    int max = tab[0];
+    int som = 0;
+    for(i = 1; i < 5; i++)
+    {
+        if(tab[i] < min)
+        min = tab[i];
+        else if(tab[i] > max)
+        max = tab[i];
+        som += tab[i];
+        
+    }
+
+    printf("min = %d , max = %d , somme = %d ",min,max,som);
+
+}
--- TP3_Ex3.c
+++ TP3_Ex3.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 int main() {
-   int i = 1;
-    while((i%i == 0) && (i % 1 == 0) )
-    {
-        printf("le nombre %d est premier ",i);
-        i++;
-    }
-
+    //Déclaration des variabes
+    int n;
+    bool premier = true;
+    int i=2;
     
-}
+    //Saisi de n positif
+    do
+    {
+        printf("entrer un nombre positif : \n");
+        scanf("%d", &n);
+    } while (n<0);
+    
+    //Vérification si n est premier ou pas 
+    while ((i<=(int) n/2)||!premier)
+    {
+        //test si i divise n ou pas 
+        if (n%i==0)
+            premier = false ;
+        i++;
+   }
+
+    //affichage du résultat
+   if (premier)
+        printf("%d est premier",n);
+    else
+        printf("%d n'est pas premier",n);
+    
+}       
